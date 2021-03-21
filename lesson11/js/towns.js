da6e3547ed2ff39d9f967fb7c437c30ac476e60e
Elvis Duru <duruelvis95@gmail.com>
2021-03-21 01:20:53 +0100

    fixed town display on hompage lesson11

--- lesson11/js/towns.js
+++ lesson11/js/towns.js
@@ -7,33 +7,36 @@ fetch(requestURL)
   .then(function (jsonObject) {
     const towns = jsonObject["towns"];
     towns.forEach((town) => {
-      let card = document.createElement("div");
-      let name = document.createElement("h2");
-      let motto = document.createElement("p");
-      let founded = document.createElement("p");
-      let population = document.createElement("p");
-      let rainfall = document.createElement("p");
-      let image = document.createElement("img");
-      let div = document.createElement("div");
+      if (["Preston", "Fish Haven", "Soda Springs"].includes(town.name)) {
+        let card = document.createElement("div");
+        let name = document.createElement("h2");
+        let motto = document.createElement("p");
+        let founded = document.createElement("p");
+        let population = document.createElement("p");
+        let rainfall = document.createElement("p");
+        let image = document.createElement("img");
+        let div = document.createElement("div");
 
-      name.textContent = town.name;
-      motto.textContent = town.motto;
-      founded.textContent = "Year Founded: " + town.yearFounded;
-      population.textContent = "Current Population: " + town.currentPopulation;
-      rainfall.textContent = "Average Rainfall: " + town.averageRainfall;
-      image.setAttribute("src", "images/" + town.photo);
-      image.setAttribute("alt", town.name);
+        name.textContent = town.name;
+        motto.textContent = town.motto;
+        founded.textContent = "Year Founded: " + town.yearFounded;
+        population.textContent =
+          "Current Population: " + town.currentPopulation;
+        rainfall.textContent = "Average Rainfall: " + town.averageRainfall;
+        image.setAttribute("src", "images/" + town.photo);
+        image.setAttribute("alt", town.name);
 
-      div.appendChild(name);
-      div.appendChild(motto);
-      div.appendChild(document.createElement("hr"));
-      div.appendChild(founded);
-      div.appendChild(population);
-      div.appendChild(rainfall);
+        div.appendChild(name);
+        div.appendChild(motto);
+        div.appendChild(document.createElement("hr"));
+        div.appendChild(founded);
+        div.appendChild(population);
+        div.appendChild(rainfall);
 
-      card.appendChild(div);
-      card.appendChild(image);
+        card.appendChild(div);
+        card.appendChild(image);
 
-      document.querySelector("#cards").appendChild(card);
+        document.querySelector("#cards").appendChild(card);
+      }
     });
   });
