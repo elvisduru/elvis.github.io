deb577514000bdb9888998af50c57efe182626ae
Elvis Duru <duruelvis95@gmail.com>
2021-04-08 10:40:59 +0100

    started events section

--- final_project/js/main.js
+++ final_project/js/main.js
@@ -5,6 +5,9 @@ WebFont.load({
   },
 });
 
+const banner = document.getElementById("banner");
+const closeBannerBtn = document.getElementById("closeBanner");
+
 // Navbar Logic
 const navBtn = document.getElementById("menuBtn");
 const closeBtn = document.getElementById("closeBtn");
@@ -24,17 +27,31 @@ const requestUrl =
 fetch(requestUrl)
   .then((response) => response.json())
   .then((jsonObject) => {
-    console.log(jsonObject);
     const currentWeather = jsonObject.current;
-    const dailyForecast = jsonObject.daily;
+    const dailyForecast = jsonObject.daily.slice(0, 3);
+    const alerts = jsonObject.alerts;
 
     document.getElementById("desc").textContent =
       currentWeather.weather[0].description;
-    document.getElementById("temp").textContent = currentWeather.temp;
+    document.getElementById("temp").textContent = currentWeather.temp + "\xB0F";
 
     document.getElementById("humidity").textContent = currentWeather.humidity;
     document.getElementById("windSpeed").textContent =
       currentWeather.wind_speed;
+
+    // Check for alerts
+    if (alerts) {
+      banner.classList.remove("hide");
+      const p = document.createElement("p");
+      p.textContent = alerts[0].description;
+      banner.appendChild(p);
+    } else {
+      banner.classList.add("hide");
+    }
+
+    closeBannerBtn.addEventListener("click", function () {
+      banner.classList.add("hide");
+    });
 
     // Weather Forecast
     const d = new Date();
@@ -50,6 +67,31 @@ fetch(requestUrl)
     weekday[5] = "Friday";
     weekday[6] = "Saturday";
 
-    const weatherList = jsonObject.list;
     let forecastDayNumber = todayDayNumber;
+
+    dailyForecast.forEach((weather) => {
+      forecastDayNumber += 1;
+      if (forecastDayNumber === 7) {
+        forecastDayNumber = 0;
+      }
+      const forecastItem = document.createElement("div");
+      forecastItem.classList = "forecast-item";
+      const dayName = document.createElement("h4");
+      dayName.textContent = weekday[forecastDayNumber];
+
+      const iconPath =
+        "//openweathermap.org/img/wn/" + weather.weather[0].icon + "@2x.png";
+      const icon = document.createElement("img");
+      icon.src = iconPath;
+      icon.alt = weather.weather[0].description;
+
+      const temp = document.createElement("p");
+      temp.textContent = weather.temp.day + "\xB0F";
+
+      forecastItem.appendChild(dayName);
+      forecastItem.appendChild(icon);
+      forecastItem.appendChild(temp);
+
+      document.querySelector(".forecast-box").appendChild(forecastItem);
+    });
   });
